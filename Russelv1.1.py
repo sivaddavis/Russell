62c887f2dfea10e763de6826ebe944314199199f
Sabrina Hsu <Sabrina@Sabrinas-MacBook-Pro.local>
2018-09-24 13:10:56 -0700

    my very first add to the repo.

---
+++ Russelv1.1.py
@@ -0,0 +1,47 @@
+import random
+import requests
+from discord import Game
+from discord.ext.commands import Bot
+
+BOT_PREFIX = ("?", "!")
+TOKEN = "NDkzNTkyMTU2OTA5NjAwNzc1.Donbrw.qRiZxTJ_Y5Hhp6RSBm0-TpVzZog"
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.command(name='1-10',
+	description="Gives you a random number from 1-10",
+	brief="Rate me",
+	aliases=['rate_me', 'hot_or_not', 'on_a_scale'],
+	pass_context=True)
+async def onetoten(context):
+	possible_responses = [
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'10',
+	]
+	await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+@client.command(name='hello',
+	description="Say hello to Russell!",
+	brief="Greet Russell, the Wilderness Explorer",
+	aliases=['hi', 'yo', 'sup'],
+	pass_context=True)
+async def hi(context):
+        await client.say("Hello " + context.message.author.mention + ", how can I assist you today?")
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('How can I assist?')
+    print('------')	
+
+client.run(TOKEN)
